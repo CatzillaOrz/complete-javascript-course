814431329b26294a0f0e4b470f31c56e74671456
CatzillaOrz <catzillaorz@outlook.com>
2022-08-08 16:36:01 +0800

    1 file modified

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ console.log(getComputedStyle(message).height);
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 ///////////////////////////////////////
 
@@ -56,4 +56,24 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+//scroll to
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y):', window.pageXOffset, pageYOffset);
+
+  console.log(
+    'heig/width viewport:',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
 });
