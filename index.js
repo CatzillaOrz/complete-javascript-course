2a89500125afcea6bd06912f55e223e23c6027e4
CatzillaOrz <catzillaorz@outlook.com>
2022-08-08 16:45:49 +0800

    1 file modified

--- index.js
+++ index.js
@@ -67,13 +67,18 @@ btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect());
+  // console.log(e.target.getBoundingClientRect());
 
   console.log('Current scroll (X/Y):', window.pageXOffset, pageYOffset);
 
-  console.log(
-    'heig/width viewport:',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
+  // console.log(
+  //   'heig/width viewport:',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  window.scrollTo(
+    s1coords.left + window.pageXOffset,
+    s1coords.top + window.pageYOffset
   );
 });
