85bc759f274b89096912e3d865654235c1070f71
CatzillaOrz <catzillaorz@outlook.com>
2022-08-08 16:48:59 +0800

    1 file modified

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+  // console.log(s1coords);
 
   // console.log(e.target.getBoundingClientRect());
 
@@ -77,8 +77,16 @@ btnScrollTo.addEventListener('click', function (e) {
   //   document.documentElement.clientWidth
   // );
 
-  window.scrollTo(
-    s1coords.left + window.pageXOffset,
-    s1coords.top + window.pageYOffset
-  );
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
