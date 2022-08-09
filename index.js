5af972b4650d2d6f34126b44aced2279789693ca
CatzillaOrz <catzillaorz@outlook.com>
2022-08-09 13:04:46 +0800

    1 file modified

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ nav.addEventListener('mouseout', handleHover.bind(1));
  */
 
 const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -169,7 +170,7 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: [0, 0.2],
-  rootMargin: '-90px',
+  rootMargin: `-${navHeight}px`,
 });
 
 headerObserver.observe(header);
