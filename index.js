a27f8087bc92faa8fdd167448b350670b1076185
CatzillaOrz <catzillaorz@outlook.com>
2022-08-15 15:26:06 +0800

    1 file modified

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      const map = L.map('map').setView([latitude, longitude], 13);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.5, -0.09])
+      L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
