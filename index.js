03a42e323c32204c793f3e4825ce98f3934dd2b4
CatzillaOrz <catzillaorz@outlook.com>
2022-08-16 17:27:50 +0800

    1 file modified

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal');
+request.send();
+request.addEventListener('load', function (data) {
+  console.log('', this.responseText);
+  const data = this.responseText;
+});
