738a1a8f59c0c2df226e1f3c4479441e5ef60c68
CatzillaOrz <catzillaorz@outlook.com>
2022-08-16 17:19:09 +0800

    4 files modified, 230 deleted

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+/*
+ **  [img-1](https://stackblitz.com/files/js-39qpnd/github/CatzillaOrz/complete-javascript-course/master/16-Asynchronous/starter/img/img-1.jpg)
+ ** [img-2](https://stackblitz.com/files/js-39qpnd/github/CatzillaOrz/complete-javascript-course/master/16-Asynchronous/starter/img/img-2.jpg)
+ ** [img-3](https://stackblitz.com/files/js-39qpnd/github/CatzillaOrz/complete-javascript-course/master/16-Asynchronous/starter/img/img-3.jpg)
+ */
+
+'use strict';
 import './style.css';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
