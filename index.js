f017c44fe99191b6635a6d7854a23f3c48947e78
CatzillaOrz <catzillaorz@outlook.com>
2022-08-22 11:04:56 +0800

    1 file modified

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ const imgContainer = document.querySelector('.images');
 ///////////////////////////////////////
 function renderCountry(data, className = '') {
   const html = `
-    <article class="country ${className}">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)}</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-      </div>
-    </article>
-    `;
+     <article class="country ${className}">
+       <img class="country__img" src="${data.flag}" />
+       <div class="country__data">
+         <h3 class="country__name">${data.name}</h3>
+         <h4 class="country__region">${data.region}</h4>
+         <p class="country__row"><span>👫</span>${(
+           +data.population / 1000000
+         ).toFixed(1)}</p>
+         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+         <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+       </div>
+     </article>
+     `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
@@ -172,3 +172,24 @@ createImg(img1)
     return wait(2);
   })
   .catch((e) => console.log('', e));
+
+const whereAmI = async function () {
+  // Geolocation
+  const pos = await getPosition();
+  const { latitude: lat, longitude: lng } = pos.coord;
+
+  // Reverse geocoding
+  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+
+  const dataGeo = await resGeo.json();
+  console.log('', dataGeo);
+
+  const res = await feth(
+    `https://restcountries.com/v2/name/${dataGeo.country}?fullText=true`
+  );
+
+  const [data] = await res.json();
+
+  console.log('', data);
+  renderCountry(data);
+};
